668c5db06454a18fe0334f85c9ea4c76ef9f47f4
Katie Jewett <kjewett17@gmail.com>
2023-04-28 15:41:49 -0400

    Dev (#3)
    * Step Tracker skeleton
    
    outlining steps and handlers
    
    * Page, Card, Step creation
    
    moving steps and handlers to Page component
    adding useCallback
    
    * CR changes
    
    * Step Tracker styling
    
    moving bg image to public
    removing vite App.css default styles
    
    * adding classnames library
    
    and implementing in step.jsx

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,10 +1,18 @@
+import classNames from 'classnames';
 import './../styles/step.css';
 
 const Step = ({step, active, currentIndex}) => {
 
+    const stepClass = classNames({
+        'step-number': true,
+        'active-background': active
+    })
+
     return (
         <>
-            <div className={"step-number"}>{active ? "I'm active!" : currentIndex + 1}</div>
+            <div className={stepClass}>{currentIndex + 1}</div>
+        {/* will uncomment these when I start working on desktop styles */}
+
             {/* <div>{step.label}</div>
             <div>{step.description}</div> */}
         </>
--- src/components/StepTracker.jsx
+++ src/components/StepTracker.jsx
@@ -3,11 +3,11 @@ import './../styles/stepTracker.css';
 
 const StepTracker = ({ steps, activeStep }) => {
   return (
-    <section>
+    <section className="step-tracker-container">
       {steps.map((step, index) => {
         return (
           <Step
-            key={index}
+            key={step.label}
             step={step}
             active={index===activeStep}
             currentIndex={index}
