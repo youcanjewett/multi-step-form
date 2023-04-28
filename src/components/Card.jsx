56e222bece4d18e9f8d47130bc6843a9c44164c1
katie <kjewett17@gmail.com>
2023-04-28 15:57:09 -0400

    card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,13 @@
+import './../styles/card.css';
+
 const Card = ({activeStep, handleNext, handleBack}) => {
     return (
-        <>
+        //will set up classname logic when I move to desktop styles
+        <div className="card-container">
             <div>card content will go here</div>
             <button onClick={handleNext}>next</button>
             <button onClick={handleBack}>back</button>
-        </>
+        </div>
     )
 }
 
