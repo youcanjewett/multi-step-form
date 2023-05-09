92e99a220c791698b7f0be3bd61bb7640a20cd11
katie <kjewett17@gmail.com>
2023-05-09 13:45:04 -0400

    CR changes and format fixes

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,51 +1,56 @@
-import React, {useState, useCallback} from 'react';
-import StepTracker from "./StepTracker"
+import React, { useState, useCallback } from "react";
+import StepTracker from "./StepTracker";
 import Card from "./Card";
-import Footer from './Footer';
-import './../index.css';
+import Footer from "./Footer";
+import "./../index.css";
 
 const Page = () => {
+ 
+  const [activeStep, setActiveStep] = useState(0);
 
-    const [activeStep, setActiveStep] = useState(0);
+  const steps = [
+    {
+      label: "Step 1",
+      description: "Your Info",
+    },
+    {
+      label: "Step 2",
+      description: "Select Plan",
+    },
+    {
+      label: "Step 3",
+      description: "Add-ons",
+    },
+    {
+      label: "Step 4",
+      description: "Summary",
+    },
+  ];
 
-    const steps = [
-        {
-            label: "Step 1",
-            description: "Your Info"
-        },
-        {
-            label: "Step 2",
-            description: "Select Plan"
-        },
-        {
-            label: "Step 3",
-            description: "Add-ons"
-        },
-        {
-            label: "Step 4",
-            description: "Summary"
-        }
-    ];
+  const handleNext = useCallback(() => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  }, []);
 
-    const handleNext = useCallback(() => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      }, []);
-    
-      const handleBack = useCallback(() => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-      }, []);
+  const handleBack = useCallback(() => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  }, []);
 
-    return (
-        <main className='page-container'>
-            <header className='header'>
-                <StepTracker steps={steps} activeStep={activeStep}/>
-            </header>
-            <Card activeStep={activeStep} />
-          
-            {/* will need logic here to only display footer component in mobile */}
-            <Footer activeStep={activeStep} handleBack={handleBack} handleNext={handleNext}/>
-        </main>
-    )
-}
+  return (
+    <main className="page-container">
+      <header className="header">
+        <StepTracker steps={steps} activeStep={activeStep} />
+      </header>
+      <Card activeStep={activeStep}/>
+
+      {/* will need logic here to only display footer component in mobile */}
+      <Footer
+        activeStep={activeStep}
+        handleBack={handleBack}
+        handleNext={handleNext}
+        
+      />
+    </main>
+  );
+};
 
 export default Page;
--- src/components/cardContent/PersonalInfo.jsx
+++ src/components/cardContent/PersonalInfo.jsx
@@ -1,35 +1,51 @@
-import StepTitle from './StepTitle';
-import StepSubtitle from './StepSubtitle';
-import './../../index.css';
+import FormTitle from "./FormTitle";
+import React, { useState } from "react";
+import "./../../styles/footer.css";
 
-const PersonalInfo = () => {
+const PersonalInfo = ({ userInfo, setUserInfo }) => {
+  const title = "Personal Info";
+  const subtitle = "Please enter your name, email address, and phone number.";
 
-    // will add state to update form object with user input
-    
-    return (
-        <form>
-            <StepTitle />
-            <StepSubtitle />
+  const [error, setError] = useState(true);
 
-            <div className="user-input">
-                {/* still need to add form validation */}
-                    <label for="name">Name</label><br/>
-                    <input type="text" id="name" placeholder="e.g. Stephen King" required/><br/>
-              
-             
-                    <label for="email">Email Address</label><br/>
-                    <input type="email" id="email" placeholder="e.g. stephenking@lorem.com"
-                        pattern=".+@globex\.com" size="30" required/><br/>
-                
-              
-                    <label for="tel">Phone Number</label><br />
-                    <input type="tel" id="tel" placeholder='e.g. +1 234 567 8901'
-                            required/><br />
-              
-                
-            </div>
-        </form>
-    )
-}
+  // will add state to update form object with user input
+
+  return (
+    <form>
+      <FormTitle title={title} subtitle={subtitle} />
+
+      <div className="user-input">
+        {/* still need to add form validation */}
+        <label for="name">Name</label>
+        {error && <span className="error">Your error message here</span>}
+        <br />
+        <input type="text" id="name" placeholder="e.g. Stephen King" required />
+        <br />
+
+        <label for="email">Email Address</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          placeholder="e.g. stephenking@lorem.com"
+          pattern=".+@globex\.com"
+          size="30"
+          required
+        />
+        <br />
+
+        <label for="tel">Phone Number</label>
+        <br />
+        <input
+          type="tel"
+          id="tel"
+          placeholder="e.g. +1 234 567 8901"
+          required
+        />
+        <br />
+      </div>
+    </form>
+  );
+};
 
 export default PersonalInfo;
